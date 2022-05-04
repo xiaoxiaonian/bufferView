7960d522ca3f8b7c1c94fca47b0ffc3e97953f48
nianrui <810199886@qq.com>
2022-05-04 20:09:26 +0800

    点击显示相关内容、说明

---
+++ lib/check.dart
@@ -0,0 +1,425 @@
+import 'package:flutter/material.dart';
+
+abstract class BufferCheck {
+  Widget? widget;
+
+  Widget? getWidget({required String source, required int index});
+}
+
+class PayloadLengthChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    int length;
+    length = int.parse(source.substring(0, 2), radix: 16);
+    if (int.parse(source.substring(2, 4), radix: 16) > 0) {
+      debugPrint("字节数量超出8位了");
+      length += int.parse(source.substring(2, 4), radix: 16) + 255;
+    }
+    length *= 2;
+    debugPrint(source);
+    widget = Row(
+      children: [
+        const Text(
+          "(Payload)字节数:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text("$length", style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class FrameLengthChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    int length;
+    length = int.parse(source.substring(0, 2), radix: 16);
+    if (int.parse(source.substring(2, 4), radix: 16) > 0) {
+      debugPrint("字节数量超出8位了");
+      length += int.parse(source.substring(2, 4), radix: 16) + 255;
+    }
+    length *= 2;
+    debugPrint(source);
+    widget = Row(
+      children: [
+        const Text(
+          "数据域长度:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text("$length", style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class FixedChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    widget = Row(
+      children: [
+        const Text(
+          "固定字节:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(source, style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class MacChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    String value = "";
+    for (int main = 10, sub = main + 2; main >= 0; main -= 2, sub -= 2) {
+      value += source.substring(main, sub);
+    }
+    widget = Row(
+      children: [
+        const Text(
+          "设备物理地址:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(value, style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class HeadChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    String head = int.parse(source, radix: 16).toRadixString(2);
+    String type = head.startsWith("1") ? "命令帧" : "应答帧";
+    String isEncrypt = head.startsWith("1", 1) ? "已加密" : "未加密";
+    widget = Row(
+      children: [
+        const Text(
+          "帧头:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text("$head  $type  $isEncrypt", style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class CmdChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    int cmd = int.parse(source, radix: 16);
+    List<String> arrTitle = ["", "-配置帧加密", "读取蓝牙卡信息", "写入蓝牙卡信息", "读取用户信息", "写入用户信息", "读取IC卡数据", "写入IC卡数据", "擦除IC卡数据", "获取IC卡密码错误计数器", "获取IC卡密码", "验证IC卡密码", "修改IC卡密码", "获取当前MTU"];
+    List<String> arrDescription = [
+      """
+          命令帧 DATA[0]：
+                  0x59 - 允许回应非加密帧
+                  0x00 - 不允许回应非加密帧
+                  特别要求：该命令帧必须加密
+          回应帧 无       
+      """,
+      """
+          命令帧 读取蓝牙卡信息，该信息保存在蓝牙芯片上，不在IC卡中
+          回应帧 读取蓝牙卡信息回应
+                    DATA[0~3]: 蓝牙卡ID，32位BCD编码，如{0x78,0x56,0x34,0x12}代表ID为
+                    12345678
+                    DATA[4]: IC卡类型
+                     0x01 – 记忆卡
+                     0x02 – 逻辑加密卡
+                     0x03 – CPU卡
+                     0x04 – 射频卡
+                    DATA[5]: IC卡型号:
+                     0x01 – SLE4442_3V3
+                     0x02 – AT88SC102_3V3
+                    DATA[6]: 蓝牙卡状态:
+                     0xFF – 初始化
+                     0xAA – 已发行
+                     0x55 – 已报废
+                    DATA[7]:电池电量百分比(0~100)
+                    DATA[8~11]:发行日期，32位BCD编码，格式YYYYMMDD，如
+                    {0x30,0x08,0x19,0x20}代表2019年8月30日
+                    DATA[12~13]: IC卡容量
+                    DATA[14~15]:Reserved
+                    DATA[16~47]: 版本信息
+      """,
+      """
+          命令帧 写入蓝牙卡信息，该信息保存在蓝牙芯片上，不在IC卡中
+                    DATA[0~3]: 蓝牙卡ID，32位BCD编码，如
+                    {0x78,0x56,0x34,0x12}代表ID为12345678
+                    DATA[4]: IC卡类型
+                     0x01 – 记忆卡
+                     0x02 – 逻辑加密卡
+                     0x03 – CPU卡
+                     0x04 – 射频卡
+                    DATA[5]: IC卡型号:
+                     0x01 – SLE4442_3V3
+                     0x02 – AT88SC102_3V3
+                    DATA[6]: 蓝牙卡状态:
+                     0xFF – 初始化
+                     0xAA – 已发行
+                     0x55 – 已报废
+                    DATA[7]:Reserved
+                    DATA[8~11]:发行日期，32位BCD编码，格式YYYYMMDD， 如{0x30,0x08,0x19,0x20}代表2019年8月30日
+                    DATA[12~13]: IC卡容量
+                    DATA[14~15]:Reserved
+                    DATA[16~47]: 版本信息
+          回应帧 无 
+      """,
+      """
+          命令帧 读取用户信息，该信息保存在蓝牙芯片上，不在IC
+                 卡中
+          回应帧 读取用户信息回应
+                DATA[0~31]: 用户信息
+      """,
+      """
+          命令帧 写入用户信息，该信息保存在蓝牙芯片上，不在IC
+                卡中
+          回应帧 写入蓝牙卡信息回应
+                DATA[0~31]: 用户信息
+      """,
+      """
+          命令帧 读取IC卡数据
+                    DATA[0]:读取区域类型
+                     0x01:主数据区
+                     0x02:保护位区
+                    DATA[1 ~ 2]:读取地址
+                    DATA[3 ~ 4]:读取长度N，N>0
+                    4442卡这两个区域均支持，主数据区共256字节，保护位
+                    区共4 字节
+                    102卡仅有主数据区，共178字节
+          回应帧 读取IC卡数据回应
+                    DATA[0]:读取区域类型
+                     0x01:主数据区
+                     0x02:保护位区
+                    DATA[1 ~ 2]:读取地址
+                    DATA[3 ~ 4]:读取长度N，N>0
+                    DATA[5 ~ 4+N]:读取的数据
+      """,
+      """
+         命令帧 写入IC卡数据
+                   DATA[0]:写入区域类型
+                    0x01:主数据区
+                    0x02:保护位区
+                   DATA[1 ~ 2]:写入地址
+                   DATA[3 ~ 4]:写入长度N，N>0
+                   DATA[5 ~ 4+N]:写入数据
+                   4442卡这两个区域均支持，主数据区共256字节，保护
+                   位区共4字节
+                   102卡仅有主数据区，共178字节
+         回应帧 写入IC卡数据回应
+                   DATA[0]:写入区域类型
+                    0x01:主数据区
+                    0x02:保护位区
+                   DATA[1 ~ 2]:写入地址
+                   DATA[3 ~ 4]:写入长度N，N>0
+      """,
+      """
+         命令帧 擦除IC卡数据
+                   DATA[0]:擦除区域类型
+                    0x01:主数据区
+                    0x02:保护位区
+                   DATA[1 ~ 2]:擦除地址
+                   DATA[3 ~ 4]:擦除长度N，N>0
+                   4442卡这两个区域均支持，主数据区共256字节，保护位区
+                   共4字节
+                   102卡仅有主数据区，共178字节
+         回应帧 擦除IC卡数据回应
+                   DATA[0]:擦除区域类型
+                    0x01:主数据区
+                    0x02:保护位区
+                   DATA[1 ~ 2]:擦除地址
+                   DATA[3 ~ 4]:擦除长度N，N>0
+      """,
+      """
+         命令帧 读取IC卡密码错误计数器
+         回应帧 读取IC卡密码错误计数器
+                   回应
+                   DATA[0]: 计数值，8位无符号数，如0x03代表
+                   错误计数为3
+      """,
+      """
+         命令帧 无
+         回应帧 4442卡：4 byte
+                DATA[0]：密钥
+                DATA[1~3]：加密后密码
+                102卡：3byte
+                DATA[0]：密钥
+                DATA[1~2]：加密后密码
+      """,
+      """
+         命令帧 4442卡：4 byte
+               DATA[0]：加密参数
+               DATA[1~3]：加密后密码
+               102卡：3byte
+               DATA[0]：加密参数
+               DATA[1~2]：加密后密码
+         回应帧 无
+      """,
+      """
+         命令帧 4442卡：8 byte
+               DATA[0]：原密码加密参数
+               DATA[1~3]：加密后原密码
+               DATA[4]：新密码加密参数
+               DATA[5~7]：加密后新密码
+               102卡：6 byte
+               DATA[0]：原密码加密参数
+               DATA[1~2]：加密后原密码
+               DATA[3]：新密码加密参数
+               DATA[4~5]：加密后新密码
+         回应帧 无
+      """,
+      """
+         命令帧 无
+         回应帧 DATA[0~1]：
+               当前MTU，16位，无符号数，小端
+      """
+    ];
+    widget = Column(
+      children: [
+        Row(
+          children: [
+            const Text(
+              "命令:",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: Text(arrTitle[cmd], style: const TextStyle(fontSize: 16)),
+              flex: 1,
+            )
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "说明:",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: Text(arrDescription[cmd], style: const TextStyle(fontSize: 16)),
+              flex: 1,
+            )
+          ],
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class ErrorChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    int error = int.parse(source, radix: 16);
+    List<String> arr = ["无错,操作成功", "未知错误", "未知命令。即该协议不支持的命令", "DATA域错误。未按命令格式传输正确的DATA", "不支持的命令。如4442与102卡均不支持写入蓝牙卡信息", "密码错误。验证、修改密码时传入的密码有误", "卡片已熔断", "电池电量过低，不允许对IC卡进行操作"];
+    widget = Row(
+      children: [
+        const Text(
+          "错误码说明:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(arr[error], style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class DataChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    widget = Row(
+      children: [
+        const Text(
+          "数据域:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(source, style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class RandomChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    widget = Row(
+      children: [
+        const Text(
+          "随机域:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(source, style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class CSChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    widget = Row(
+      children: [
+        const Text(
+          "校验和:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(source, style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
+
+class EndChecker extends BufferCheck {
+  @override
+  Widget? getWidget({required String source, required int index}) {
+    widget = Row(
+      children: [
+        const Text(
+          "结束位:",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(source, style: const TextStyle(fontSize: 16)),
+          flex: 1,
+        )
+      ],
+    );
+    return widget;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:plot/bean.dart';
+import 'package:plot/check.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-// region  ---- symbol:分包标志位  payloadLength:当前数据包携带的数据字节数，16位，无符号，小端  start:起始位  head:帧头  mac:MAC地址  cmd:命令域  err:错误码  frameLength:数据域长度  data:数据域  random:随机域  cs:校验和  ----
-enum BufferType { symbol, payloadLength, start, head, mac, cmd, err, frameLength, data, random, cs, end }
+// region  ---- symbol:分包标志位  payloadLength:当前数据包携带的数据字节数，16位，无符号，小端  fixed:固定字节  head:帧头  mac:MAC地址  cmd:命令域  err:错误码  frameLength:数据域长度  data:数据域  random:随机域  cs:校验和  ----
+enum BufferType { symbol, payloadLength, fixed, head, mac, cmd, err, empty, frameLength, data, random, cs, end }
 // endregion
 
 class MyApp extends StatelessWidget {
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
@@ -37,32 +38,31 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int dataLength = 0;
-
-  // String? buffer;
+  String? buffer;
 
   // String buffer = "0F0FA50006A200000864330AFFFF000000ED03FFFFFF3F8AAA00FE00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000058ED010203040506FFFFFFFFFFFF3289EF41571C3F8A1FFE01E100000864FFFF0A0000000000000000000100000005000102612CED032359A37C44279EA5475589CABBFE164ACFAE01020304050600000000001C442937CA";
-  String? buffer = "001900aa0055d1532e3503bae9060005000100004000f3755e06e516";
-
+  // String? buffer = "001900aac055d1532e3503bae90a0305000100004000f3755e06e516";
   // String? buffer ="00B900AAC055D1532E3503BAE95F59FC59585959F9595656035958FB5959512B750EA6A6A6A659A6A6A6A6A666D30C5959A6A6A6A6A659595959AB5A59593D595959D3585959D1A6A6A6A6A6A6A6DEA6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A6A66BD0B6180E4566D351595A595959512B3D595959B15A595953595959535959595C59595962EE5B4E59595959595959595959595959595959D33559599D8B644E59595959599E1D706E935CEE9CF97816";
   List<RegBean> list = [];
   RegExp regExp0 = RegExp(r"^\w{6}aa\w{2}55\w{12}e9");
   List<Map<String, dynamic>> arrRegExp = [
     {"RegExp": RegExp(r"^\w{2}"), "type": BufferType.symbol.index},
     {"RegExp": RegExp(r"(?<=^\w{2})\w{4}"), "type": BufferType.payloadLength.index},
-    {"RegExp": RegExp(r"(?<=^\w{6})aa"), "type": BufferType.start.index},
+    {"RegExp": RegExp(r"(?<=^\w{6})aa"), "type": BufferType.fixed.index},
     {"RegExp": RegExp(r"(?<=^\w{8})\w{2}"), "type": BufferType.head.index},
-    {"RegExp": RegExp(r"(?<=^\w{10})55"), "type": BufferType.start.index},
+    {"RegExp": RegExp(r"(?<=^\w{10})55"), "type": BufferType.fixed.index},
     {"RegExp": RegExp(r"(?<=^\w{12})\w{12}"), "type": BufferType.mac.index},
-    {"RegExp": RegExp(r"(?<=^\w{24})e9"), "type": BufferType.start.index},
+    {"RegExp": RegExp(r"(?<=^\w{24})e9"), "type": BufferType.fixed.index},
     {"RegExp": RegExp(r"(?<=^\w{26})\w{2}"), "type": BufferType.cmd.index},
-    {"RegExp": RegExp(r"(?<=^\w{28})\w{2}"), "type": -1},
+    {"RegExp": RegExp(r"(?<=^\w{28})\w{2}"), "type": BufferType.err.index},
     {"RegExp": RegExp(r"(?<=^\w{30})\w{4}"), "type": BufferType.frameLength.index},
     {"RegExp": RegExp(r"(?<=^\w{34})\w+(?=\w{12}$)"), "type": BufferType.data.index},
     {"RegExp": RegExp(r"\w{8}(?=\w{4}$)"), "type": BufferType.random.index},
     {"RegExp": RegExp(r"\w{2}(?=\w{2}$)"), "type": BufferType.cs.index},
     {"RegExp": RegExp(r"16$"), "type": BufferType.end.index},
   ];
-  List<Color> arrColor = [Colors.blue, Colors.amberAccent, Colors.cyan, Colors.pinkAccent, Colors.brown, Colors.lightGreen, const Color(0xffff0000), const Color(0xff00ff00), Colors.black, Colors.blueGrey, Colors.indigoAccent, Colors.purpleAccent];
+  List<Color> arrColor = [Colors.blue, Colors.amberAccent, Colors.cyan, Colors.pinkAccent, Colors.brown, Colors.deepOrange, const Color(0xff00ff00), const Color(0xffff0000), Colors.amberAccent, Colors.black, Colors.blueGrey, Colors.indigoAccent, Colors.purpleAccent];
+  Widget? extentWidget;
 
   @override
   void initState() {
@@ -74,18 +74,14 @@ class _MyHomePageState extends State<MyHomePage> {
     String? source;
     buffer = buffer?.toLowerCase();
     if (buffer != null && regExp0.hasMatch(buffer!)) {
+      list.clear();
       debugPrint("hasMatch");
       for (var element in arrRegExp) {
         source = element["RegExp"].stringMatch(buffer);
         debugPrint("$source");
         if ((element["RegExp"] as RegExp).pattern == r"(?<=^\w{28})\w{2}") {
-          putBuffer(source ?? "", source == "00" ? 7 : 6);
-        }
-        /*else if((element["RegExp"] as RegExp).pattern == r"(?<=^\w{34})")
-          {
-
-          }*/
-        else {
+          putBuffer(value: source ?? "", type: source == "00" ? 6 : 7);
+        } else {
           if ((element["RegExp"] as RegExp).pattern == r"(?<=^\w{30})\w{4}") {
             debugPrint("数据长度");
             dataLength = int.parse(source!.substring(0, 2), radix: 16);
@@ -95,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
             }
             debugPrint("$dataLength");
           }
-          putBuffer(source ?? "", element["type"]);
+          putBuffer(value: source ?? "", type: element["type"]);
         }
       }
     } else {
@@ -104,81 +100,147 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {});
   }
 
-  void putBuffer(String value, int type) {
-    value.characters.forEach((element) {
-      list.add(RegBean.fromParams(name: element, color: arrColor[type]));
-    });
-
-    // List<String> arrayListTarget = arr.map((e) => buffer.substring(e.start, e.end)).toList();
+  void putBuffer({required String value, required int type}) {
+    for (var element in value.characters) {
+      list.add(RegBean.fromParams(name: element, color: arrColor[type], type: type));
+    }
+    // List<String> arrayListTarget = arr.map((e) => buffer.substring(e.fixed, e.end)).toList();
     // list = arrayListTarget.map((e) => RegBean.fromParams(name: e)).toList();
+  }
+
+  void onBufferTap({required int index}) {
+    debugPrint("onBufferTap :$index");
+    // debugPrint("$list");
+    BufferCheck? bufferCheck;
+    String source = "";
+    BufferType type = BufferType.values.singleWhere((element) => element.index == list[index].type);
+    Iterable iterable = list.where((element) => element.type == type.index);
+    for (var element in iterable) {
+      source += (element as RegBean).name ?? "";
+    }
+    debugPrint("tap :$type");
+    switch (type) {
+      case BufferType.symbol:
+        break;
+      case BufferType.payloadLength:
+        debugPrint("");
+        bufferCheck = PayloadLengthChecker();
+        break;
+      case BufferType.fixed:
+        bufferCheck = FixedChecker();
+        break;
+      case BufferType.head:
+        bufferCheck = HeadChecker();
+        break;
+      case BufferType.mac:
+        bufferCheck = MacChecker();
+        break;
+      case BufferType.cmd:
+        bufferCheck = CmdChecker();
+        break;
+      case BufferType.err:
+        bufferCheck = ErrorChecker();
+        break;
+      case BufferType.empty:
+        bufferCheck = ErrorChecker();
+        break;
+      case BufferType.frameLength:
+        bufferCheck = FrameLengthChecker();
+        break;
+      case BufferType.data:
+        bufferCheck = DataChecker();
+        break;
+      case BufferType.random:
+        bufferCheck = RandomChecker();
+        break;
+      case BufferType.cs:
+        bufferCheck = CSChecker();
+        break;
+      case BufferType.end:
+        bufferCheck = EndChecker();
+        break;
+      default:
+    }
+    if (source.isNotEmpty) {
+      extentWidget = bufferCheck?.getWidget(source: source, index: index);
+    }
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Container(
-        alignment: Alignment.center,
-        color: Colors.blueGrey,
+        alignment: Alignment.topLeft,
+        color: Colors.white,
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Stack(children: [
-          SingleChildScrollView(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 50),
-              Wrap(
-                children: list
-                    .asMap()
-                    .keys
-                    .map((e) => FractionallySizedBox(
-                          alignment: Alignment.center,
-                          widthFactor: 0.06,
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30, right: 50),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Clipboard.getData(Clipboard.kTextPlain).then((value) {
+                          debugPrint("粘贴的数据->${value?.text}");
+                          buffer = value?.text;
+                          if (buffer == null) {
+                            debugPrint("空数据");
+                          } else {
+                            if (buffer!.isNotEmpty) {
+                              updateContent();
+                            }
+                          }
+                        });
+                      },
+                      child: Icon(list.isEmpty ? Icons.add : Icons.refresh_outlined)),
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+                child: SingleChildScrollView(
+                    child: Column(
+              children: [
+                Wrap(
+                  children: list
+                      .asMap()
+                      .keys
+                      .map((e) => FractionallySizedBox(
                             alignment: Alignment.center,
-                            color: list[e].color,
-                            width: MediaQuery.of(context).size.width,
-                            // height: MediaQuery.of(context).size.height,
-                            child: Text(
-                              list[e].name ?? "",
-                            ),
-                          ),
-                        ))
-                    .toList(),
-              )
-            ],
-          )),
-          Positioned(
-            child: ElevatedButton(
-                onPressed: () {
-                  Clipboard.getData(Clipboard.kTextPlain).then((value) {
-                    debugPrint("粘贴的数据->${value?.text}");
-                    buffer = value?.text;
-                    if (buffer == null) {
-                      debugPrint("空数据");
-                    } else {
-                      if (buffer!.isNotEmpty) {
-                        updateContent();
-                      }
-                    }
-                  });
-                },
-                child: Icon(list.isEmpty ? Icons.add : Icons.refresh_outlined)),
-            top: 5,
-            right: 0,
-          )
-        ]),
+                            widthFactor: 0.06,
+                            child: GestureDetector(
+                                onTap: () {
+                                  onBufferTap(index: e);
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(vertical: 5),
+                                  alignment: Alignment.center,
+                                  color: list[e].color,
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Text(
+                                    list[e].name ?? "",
+                                  ),
+                                )),
+                          ))
+                      .toList(),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: extentWidget ??
+                      Container(
+                        child: null,
+                      ),
+                )
+              ],
+            )))
+          ],
+        ),
       ),
-      /* floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), */ // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
